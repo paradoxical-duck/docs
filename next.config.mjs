b41e53581c11412d4166e9be9450c25902d58610
awesumdude42 <sammyiyers@gmail.com>
2026-05-31 22:40:53 -0700

    ok so... i added a bunch math to the B-Spline theory file! i explained how matrix form works!

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ import nextra from 'nextra'
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
-  latex:{
-    renderer: 'mathjax'
-  }
+  latex:{ 
+      renderer: 'mathjax'
+   }
 })
  
 const nextConfig = {
